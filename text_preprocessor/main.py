0260ca8adc09394447ab984ddc72e39f3ebdd260
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-05-18 16:28:24 -0400

    Create main.py

---
+++ text_preprocessor/main.py
@@ -0,0 +1,23 @@
+from preprocessor import PreProcessor
+from pipeline import Pipeline
+
+
+def main():
+    # Create the instances of PreProcessor and Pipeline
+    preprocessor = PreProcessor()
+    pipeline = preprocessor.create_pipeline()
+
+    # Add preprocessing methods to the pipeline
+    pipeline.add_method([preprocessor.make_lowercase,
+                         preprocessor.check_spelling,
+                         preprocessor.remove_names,
+                         preprocessor.remove_whitespace])
+
+    # Use the pipeline to preprocess the text
+    text = ' Helllo, I am John Doe!!! My EMAIL is john.doe@email.com. ViSIT ouR wEbSite www.johndoe.com '
+    processed_text = pipeline.execute_pipeline(text)
+    print(processed_text)
+
+
+if __name__ == "__main__":
+    main()

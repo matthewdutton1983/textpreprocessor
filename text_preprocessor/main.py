82ff9e597a570ea94f4265794312808cd2455efe
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-05-24 13:31:16 -0400

    Update main.py

--- text_preprocessor/main.py
+++ text_preprocessor/main.py
@@ -2,22 +2,11 @@ from .pipeline_manager import PreProcessor
 
 
 def main():
-    # Create the instances of PreProcessor and Pipeline
     preprocessor = PreProcessor()
-    pipeline = preprocessor.create_pipeline()
-
-    # Add preprocessing methods to the pipeline
-    # TODO: Fix this
-    pipeline.add_methods([preprocessor.make_lowercase,
-                         preprocessor.check_spelling,
-                         preprocessor.remove_names,
-                         preprocessor.remove_whitespace])
-
-    # Use the pipeline to preprocess the text
+    pipeline = preprocessor.create_pipeline(load_defaults=True)
     text = ' Helllo, I am John Doe!!! My EMAIL is john.doe@email.com. ViSIT ouR wEbSite www.johndoe.com '
-    processed_text = pipeline.execute_pipeline(text)
-    print(processed_text)
-
+    result = pipeline.execute_pipeline(text)
+    print(result)
 
 if __name__ == "__main__":
     main()

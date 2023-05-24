111fb1d2d42a9b351edc6f6739e0daf468d254d2
matthewdutton1983 <79879742+matthewdutton1983@users.noreply.github.com>
2023-05-24 13:32:22 -0400

    Update pipeline_methods.py

--- text_preprocessor/pipeline_methods.py
+++ text_preprocessor/pipeline_methods.py
@@ -17,98 +17,9 @@ from spellchecker import SpellChecker
 from text_preprocessor.decorators import pipeline_method
 
 
+@classmethod
 @pipeline_method
-def replace_words(input_text: str, replacement_dict: Dict[str, str], case_sensitive: bool = False) -> str:
-    if case_sensitive:
-        regex_pattern = re.compile(
-            r'\b(' + '|'.join(replacement_dict.keys()) + r')\b')
-    else:
-        regex_pattern = re.compile(
-            r'\b(' + '|'.join(replacement_dict.keys()) + r')\b', re.IGNORECASE)
-    return regex_pattern.sub(lambda x: replacement_dict[x.group()], input_text)
-
-
-@pipeline_method
-def make_lowercase(input_text: str) -> str:
-    return input_text.lower()
-
-
-@pipeline_method
-def make_uppercase(input_text: str) -> str:
-    return input_text.upper()
-
-
-@pipeline_method
-def remove_numbers(input_text: str) -> str:
-    return re.sub('\d+', '', input_text)
-
-
-@pipeline_method
-def remove_list_markers(input_text: str) -> str:
-    """Remove bullets or numbering in itemized input"""
-    return re.sub('(^|\s)[0-9a-zA-Z][.)]\s+|(^|\s)[ivxIVX]+[.)]\s+', ' ', input_text)
-
-
-@pipeline_method
-def remove_urls(input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
-    regex_pattern = re.compile(r'(www|http)\S+')
-    if custom_mask:
-        use_mask = True
-        mask = custom_mask
-    else:
-        mask = '<URL>'
-    return regex_pattern.sub(mask if use_mask else '', input_text)
-
-
-@pipeline_method
-def remove_punctuation(input_text: str, punctuations: Optional[str] = None) -> str:
-    """
-    Removes all punctuations from a string as defined by string.punctuation or a custom list.
-    For reference, Python's string.punctuation is equivalent to '!"#$%&\'()*+,-./:;<=>?@[\\]^_{|}~'
-    """
-    if punctuations is None:
-        punctuations = string.punctuation
-    return input_text.translate(str.maketrans('', '', punctuations))
-
-
-@pipeline_method
-def remove_duplicate_punctuation(input_text: str) -> str:
-    return re.sub(r'([\!\?\.\,\:\;]){2,}', r'\1', input_text)
-
-
-@pipeline_method
-def remove_special_characters(input_text: str, remove_unicode: bool = False, custom_characters: Optional[List[str]] = None) -> str:
-    if remove_unicode:
-        processed_text = re.sub(r'[^\w\s]', '', input_text)
-        processed_text = ''.join(
-            char for char in processed_text if ord(char) < 128)
-    elif custom_characters is not None:
-        for character in custom_characters:
-            processed_text = input_text.replace(character, '')
-    else:
-        processed_text = re.sub(r'[^\w\s]', '', input_text)
-    return processed_text
-
-
-@pipeline_method
-def expand_contractions(input_text: str) -> str:
-    return contractions.fix(input_text)
-
-
-@pipeline_method
-def remove_email_addresses(input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
-    regex_pattern = re.compile(
-        r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}')
-    if custom_mask:
-        use_mask = True
-        mask = custom_mask
-    else:
-        mask = '<EMAIL_ADDRESS>'
-    return regex_pattern.sub(mask if use_mask else '', input_text)
-
-
-@pipeline_method
-def check_spelling(input_text_or_list: Union[str, List[str]], case_sensitive: bool = True, language: str = 'en') -> str:
+def check_spelling(cls, input_text_or_list: Union[str, List[str]], case_sensitive: bool = True, language: str = 'en') -> str:
     supported_languages = ['en', 'es', 'fr', 'pt', 'de', 'ru', 'ar']
     if language not in supported_languages:
         raise ValueError(
@@ -136,50 +47,129 @@ def check_spelling(input_text_or_list: Union[str, List[str]], case_sensitive: bo
 
     return ' '.join(corrected_tokens).strip()
 
-
+@classmethod
 @pipeline_method
-def remove_stopwords(self, input_text_or_list: Union[str, List[str]], stop_words: Optional[set] = None) -> List[str]:
-    try:
-        if stop_words is None:
-            stop_words = set(stopwords.words('english'))
-        if isinstance(stop_words, list):
-            stop_words = set(stop_words)
-        if isinstance(input_text_or_list, str):
-            tokens = word_tokenize(input_text_or_list)
-            processed_tokens = [
-                token for token in tokens if token not in stop_words]
-        else:
-            processed_tokens = [token for token in input_text_or_list
-                                if (token not in stop_words and token is not None and len(token) > 0)]
-        return processed_tokens
-    except Exception as e:
-        self._log_error(e)
+def encode_text(cls, input_text: str, encoding: str = 'utf-8', errors: str = 'strict') -> bytes:
+    if encoding not in ['utf-8', 'ascii']:
+        raise ValueError(
+            "Invalid encoding type. Only 'utf-8' and 'ascii' are supported.")
+    if errors not in ['strict', 'ignore', 'replace']:
+        raise ValueError(
+            "Invalid error handling strategy. Only 'strict', 'ignore', and 'replace' are supported.")
+    processed_text = input_text.encode(encoding, errors=errors)
+    return processed_text
 
-
+@classmethod
 @pipeline_method
-def tokenize_words(input_text: str) -> List[str]:
-    """Converts a text into a list of word tokens"""
-    if input_text is None or len(input_text) == 0:
-        return []
-    return word_tokenize(input_text)
+def expand_contractions(cls, input_text: str) -> str:
+    return contractions.fix(input_text)
 
-
+@classmethod
 @pipeline_method
-def tokenize_sentences(input_text: str) -> List[str]:
-    """Converts a text into a list of sentence tokens"""
-    if input_text is None or len(input_text) == 0:
-        return []
-    return [str(s) for s in sent_tokenize(input_text)]
+def find_abbreviations(cls, input_text: str) -> Dict[str, str]:
+    abbrevs = schwartz_hearst.extract_abbreviation_definition_pairs(
+        doc_text=input_text)
+    abbreviations_dict = {
+        abbreviation: definition for abbreviation, definition in abbrevs}
+    return abbreviations_dict
 
-
+@classmethod
 @pipeline_method
-def normalize_unicode(input_text: str) -> str:
+def handle_line_feeds(cls, input_text: str, mode: str = 'remove') -> str:
+    """Handle line feeds in the input text"""
+    if mode == 'remove':
+        processed_text = input_text.replace('\n', '').replace('\r', '')
+        return processed_text
+    elif mode == 'crlf':
+        processed_text = input_text.replace(
+            '\n', '\r\n').replace('\r\r\n', '\r\n')
+        return processed_text
+    elif mode == 'lf':
+        processed_text = input_text.replace(
+            '\r\n', '\n').replace('\r', '\n')
+        return processed_text
+    else:
+        raise ValueError(
+            f"Invalid mode: '{mode}'. Options are 'remove', 'crlf', and 'lf'.")
+    
+@classmethod
+@pipeline_method
+def lemmatize_words(cls, input_text_or_list: Union[str, List[str]]) -> List[str]:
+    """Lemmatize each token in a text by finding its base form"""
+    lemmatizer = WordNetLemmatizer()
+
+    if isinstance(input_text_or_list, str):
+        tokens = word_tokenize(input_text_or_list)
+        processed_tokens = [lemmatizer.lemmatize(
+            token) for token in tokens]
+    else:
+        processed_tokens = [lemmatizer.lemmatize(token) for token in input_text_or_list
+                            if token is not None and len(token) > 0]
+    return processed_tokens
+
+@classmethod
+@pipeline_method
+def make_lowercase(cls, input_text: str) -> str:
+    return input_text.lower()
+
+@classmethod
+@pipeline_method
+def make_uppercase(cls, input_text: str) -> str:
+    return input_text.upper()
+
+@classmethod
+@pipeline_method
+def normalize_unicode(cls, input_text: str) -> str:
     """Normalize unicode data to remove umlauts, and accents, etc."""
     return normalize('NFKD', input_text).encode('ASCII', 'ignore').decode('utf-8')
 
-
+@classmethod
 @pipeline_method
-def remove_names(input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
+def replace_words(cls, input_text: str, replacement_dict: Dict[str, str], case_sensitive: bool = False) -> str:
+    if case_sensitive:
+        regex_pattern = re.compile(
+            r'\b(' + '|'.join(replacement_dict.keys()) + r')\b')
+    else:
+        regex_pattern = re.compile(
+            r'\b(' + '|'.join(replacement_dict.keys()) + r')\b', re.IGNORECASE)
+    return regex_pattern.sub(lambda x: replacement_dict[x.group()], input_text)
+
+@classmethod
+@pipeline_method
+def remove_credit_card_numbers(cls, input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
+    regex_pattern = '(4[0-9]{12}(?:[0-9]{3})?|(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][' \
+                    '0-9]|2720)[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(' \
+                    '?:2131|1800|35\d{3})\d{11})'
+    mask = '<CREDIT_CARD_NUMBER>' if use_mask else ''
+    if custom_mask is not None:
+        mask = custom_mask
+    return re.sub(regex_pattern, mask, input_text)
+@classmethod
+@pipeline_method
+def remove_duplicate_punctuation(cls, input_text: str) -> str:
+    return re.sub(r'([\!\?\.\,\:\;]){2,}', r'\1', input_text)
+
+@classmethod
+@pipeline_method
+def remove_email_addresses(cls, input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
+    regex_pattern = re.compile(
+        r'[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}')
+    if custom_mask:
+        use_mask = True
+        mask = custom_mask
+    else:
+        mask = '<EMAIL_ADDRESS>'
+    return regex_pattern.sub(mask if use_mask else '', input_text)
+
+@classmethod
+@pipeline_method
+def remove_list_markers(cls, input_text: str) -> str:
+    """Remove bullets or numbering in itemized input"""
+    return re.sub('(^|\s)[0-9a-zA-Z][.)]\s+|(^|\s)[ivxIVX]+[.)]\s+', ' ', input_text)
+
+@classmethod
+@pipeline_method
+def remove_names(cls, input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
     if custom_mask:
         use_mask = True
         mask = custom_mask
@@ -205,9 +195,87 @@ def remove_names(input_text: str, use_mask: Optional[bool] = True, custom_mask: 
 
     return ''.join([(t if t in string.punctuation else ' ' + t) for t in tokens]).strip()
 
-
+@classmethod
 @pipeline_method
-def remove_whitespace(input_text: str, mode: str = 'strip', keep_duplicates: bool = False) -> str:
+def remove_numbers(cls, input_text: str) -> str:
+    return re.sub('\d+', '', input_text)
+
+@classmethod
+@pipeline_method
+def remove_phone_numbers(cls, input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
+    regex_pattern = re.compile(
+        r'(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?(?!\d)')
+    mask = custom_mask if custom_mask else '<PHONE_NUMBER>'
+
+    if use_mask:
+        return regex_pattern.sub(lambda match: ' ' + mask if match.group().startswith(' ') else mask, input_text)
+    else:
+        return regex_pattern.sub('', input_text)
+    
+@classmethod
+@pipeline_method
+def remove_punctuation(cls, input_text: str, punctuations: Optional[str] = None) -> str:
+    """
+    Removes all punctuations from a string as defined by string.punctuation or a custom list.
+    For reference, Python's string.punctuation is equivalent to '!"#$%&\'()*+,-./:;<=>?@[\\]^_{|}~'
+    """
+    if punctuations is None:
+        punctuations = string.punctuation
+    return input_text.translate(str.maketrans('', '', punctuations))
+
+@classmethod
+@pipeline_method
+def remove_social_security_numbers(cls, input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
+    """Regex pattern follows the rules for a valid SSN"""
+    regex_pattern = '(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}|(' \
+                    '?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}'
+    mask = custom_mask or '<SOCIAL_SECURITY_NUMBER>'
+    return re.sub(regex_pattern, mask if use_mask else '', input_text)
+
+@classmethod
+@pipeline_method
+def remove_special_characters(cls, input_text: str, remove_unicode: bool = False, custom_characters: Optional[List[str]] = None) -> str:
+    if remove_unicode:
+        processed_text = re.sub(r'[^\w\s]', '', input_text)
+        processed_text = ''.join(
+            char for char in processed_text if ord(char) < 128)
+    elif custom_characters is not None:
+        for character in custom_characters:
+            processed_text = input_text.replace(character, '')
+    else:
+        processed_text = re.sub(r'[^\w\s]', '', input_text)
+    return processed_text
+
+@classmethod
+@pipeline_method
+def remove_stopwords(cls, input_text_or_list: Union[str, List[str]], stop_words: Optional[set] = None) -> List[str]:
+    if stop_words is None:
+        stop_words = set(stopwords.words('english'))
+    if isinstance(stop_words, list):
+        stop_words = set(stop_words)
+    if isinstance(input_text_or_list, str):
+        tokens = word_tokenize(input_text_or_list)
+        processed_tokens = [
+            token for token in tokens if token not in stop_words]
+    else:
+        processed_tokens = [token for token in input_text_or_list
+                            if (token not in stop_words and token is not None and len(token) > 0)]
+    return processed_tokens
+
+@classmethod
+@pipeline_method
+def remove_urls(cls, input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
+    regex_pattern = re.compile(r'(www|http)\S+')
+    if custom_mask:
+        use_mask = True
+        mask = custom_mask
+    else:
+        mask = '<URL>'
+    return regex_pattern.sub(mask if use_mask else '', input_text)
+
+@classmethod
+@pipeline_method
+def remove_whitespace(cls, input_text: str, mode: str = 'strip', keep_duplicates: bool = False) -> str:
     """Remove leading and trailing spaces by default, as well as duplicate whitespace."""
     if mode == 'leading':
         processed_text = re.sub(
@@ -231,53 +299,9 @@ def remove_whitespace(input_text: str, mode: str = 'strip', keep_duplicates: boo
 
     return processed_text
 
-
+@classmethod
 @pipeline_method
-def remove_phone_numbers(input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
-    regex_pattern = re.compile(
-        r'(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?(?!\d)')
-    mask = custom_mask if custom_mask else '<PHONE_NUMBER>'
-
-    if use_mask:
-        return regex_pattern.sub(lambda match: ' ' + mask if match.group().startswith(' ') else mask, input_text)
-    else:
-        return regex_pattern.sub('', input_text)
-
-
-@pipeline_method
-def encode_text(input_text: str, encoding: str = 'utf-8', errors: str = 'strict') -> bytes:
-    if encoding not in ['utf-8', 'ascii']:
-        raise ValueError(
-            "Invalid encoding type. Only 'utf-8' and 'ascii' are supported.")
-    if errors not in ['strict', 'ignore', 'replace']:
-        raise ValueError(
-            "Invalid error handling strategy. Only 'strict', 'ignore', and 'replace' are supported.")
-    processed_text = input_text.encode(encoding, errors=errors)
-    return processed_text
-
-
-@pipeline_method
-def remove_social_security_numbers(input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
-    """Regex pattern follows the rules for a valid SSN"""
-    regex_pattern = '(?!219-09-9999|078-05-1120)(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}|(' \
-                    '?!219099999|078051120)(?!666|000|9\d{2})\d{3}(?!00)\d{2}(?!0{4})\d{4}'
-    mask = custom_mask or '<SOCIAL_SECURITY_NUMBER>'
-    return re.sub(regex_pattern, mask if use_mask else '', input_text)
-
-
-@pipeline_method
-def remove_credit_card_numbers(input_text: str, use_mask: Optional[bool] = True, custom_mask: Optional[str] = None) -> str:
-    regex_pattern = '(4[0-9]{12}(?:[0-9]{3})?|(?:5[1-5][0-9]{2}|222[1-9]|22[3-9][0-9]|2[3-6][0-9]{2}|27[01][' \
-                    '0-9]|2720)[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|6(?:011|5[0-9]{2})[0-9]{12}|(' \
-                    '?:2131|1800|35\d{3})\d{11})'
-    mask = '<CREDIT_CARD_NUMBER>' if use_mask else ''
-    if custom_mask is not None:
-        mask = custom_mask
-    return re.sub(regex_pattern, mask, input_text)
-
-
-@pipeline_method
-def stem_words(input_text_or_list: Union[str, List[str]], stemmer: Optional[str] = None) -> List[str]:
+def stem_words(cls, input_text_or_list: Union[str, List[str]], stemmer: Optional[str] = None) -> List[str]:
     """Stem each token in a text"""
     supported_stemmers = {
         'snowball': SnowballStemmer('english'),
@@ -301,45 +325,18 @@ def stem_words(input_text_or_list: Union[str, List[str]], stemmer: Optional[str]
                             if token is not None and len(token) > 0]
     return processed_tokens
 
-
+@classmethod
 @pipeline_method
-def lemmatize_words(input_text_or_list: Union[str, List[str]]) -> List[str]:
-    """Lemmatize each token in a text by finding its base form"""
-    lemmatizer = WordNetLemmatizer()
+def tokenize_sentences(cls, input_text: str) -> List[str]:
+    """Converts a text into a list of sentence tokens"""
+    if input_text is None or len(input_text) == 0:
+        return []
+    return [str(s) for s in sent_tokenize(input_text)]
 
-    if isinstance(input_text_or_list, str):
-        tokens = word_tokenize(input_text_or_list)
-        processed_tokens = [lemmatizer.lemmatize(
-            token) for token in tokens]
-    else:
-        processed_tokens = [lemmatizer.lemmatize(token) for token in input_text_or_list
-                            if token is not None and len(token) > 0]
-    return processed_tokens
-
-
+@classmethod
 @pipeline_method
-def handle_line_feeds(input_text: str, mode: str = 'remove') -> str:
-    """Handle line feeds in the input text"""
-    if mode == 'remove':
-        processed_text = input_text.replace('\n', '').replace('\r', '')
-        return processed_text
-    elif mode == 'crlf':
-        processed_text = input_text.replace(
-            '\n', '\r\n').replace('\r\r\n', '\r\n')
-        return processed_text
-    elif mode == 'lf':
-        processed_text = input_text.replace(
-            '\r\n', '\n').replace('\r', '\n')
-        return processed_text
-    else:
-        raise ValueError(
-            f"Invalid mode: '{mode}'. Options are 'remove', 'crlf', and 'lf'.")
-
-
-@pipeline_method
-def find_abbreviations(input_text: str) -> Dict[str, str]:
-    abbrevs = schwartz_hearst.extract_abbreviation_definition_pairs(
-        doc_text=input_text)
-    abbreviations_dict = {
-        abbreviation: definition for abbreviation, definition in abbrevs}
-    return abbreviations_dict
+def tokenize_words(cls, input_text: str) -> List[str]:
+    """Converts a text into a list of word tokens"""
+    if input_text is None or len(input_text) == 0:
+        return []
+    return word_tokenize(input_text)
